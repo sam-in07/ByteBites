511cd7a744809ad51df2dd1e3e34f121621cb69b
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-04 14:46:27 +0600

    commit upades

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import Navbar from './pages/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import "./Navbar.css"
+import { assets } from "../../assets/assets"
+import { NavLink } from "react-router-dom"
+import { useStore } from "../../Context/StoreContext"
+
+const Navbar = () => {
+  const { cartCount } = useStore()
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="ByteBites Logo" />
+      <ul className="navbar-menu">
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/menu" className={({ isActive }) => (isActive ? "active" : "")}>Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to="/app" className={({ isActive }) => (isActive ? "active" : "")}>Mobile App</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>Contact Us</NavLink>
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search Icon" />
+        <div className="navbar-search-icon">
+          <NavLink to="/cart">
+            <img src={assets.basket_icon} alt="Cart" />
+            {cartCount > 0 && <div className="dot">{cartCount}</div>}
+          </NavLink>
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
