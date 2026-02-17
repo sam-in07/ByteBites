303f6d9b80ddcc4056883279d443c61ba8e3e322
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-17 11:12:23 +0600

    footer updTE

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import "./Footer.css";
 import { assets } from "../../assets/assets";
+
 const Footer = () => {
   return (
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio corporis quia quam laborum fuga error ipsa eveniet ipsum dolore ratione hic accusantium obcaecati quaerat culpa totam quibusdam, aliquid voluptatem cupiditate.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            corporis quia quam laborum fuga error ipsa eveniet ipsum dolore
+            ratione hic accusantium obcaecati quaerat culpa totam quibusdam,
+            aliquid voluptatem cupiditate.
+          </p>
+          <div className="footer-social-icons">
+            {/* div.x  & img*n   */}
 
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
-        <div className="footer-content-center"></div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivary</li>
+            <li>Privacy Ploicy</li>
+          </ul>
+        </div>
 
-        <div className="footer-content-right"></div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+01858756867</li>
+            <li>chainsamin@gmail.com</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
