884ef811b049dc49ad024c4ba47e7c9b7fe8ede7
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-24 13:50:12 +0600

    storecontext issue fixed

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,17 +1,15 @@
-import React, { useContext } from 'react'
-import './Cart.css'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../Context/StoreContext";
 export const Cart = () => {
-  const {cartItems,food_list,removeFromCart}=useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
-    <div className='cart'>
-
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
           <p>Title</p>
-          <p>
-            Price
-          </p>
+          <p>Price</p>
           <p>Quantity</p>
           <p>Total</p>
           <p>Remove</p>
@@ -19,7 +17,6 @@ export const Cart = () => {
         <br />
         <hr />
       </div>
-
     </div>
-  )
-}
+  );
+};
