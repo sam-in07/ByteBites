bd92c7c129da294ac10eee92f29c7eebc8c7d6c7
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-26 12:21:04 +0600

    promocode Cart tablees complte

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -23,10 +23,12 @@ export const Cart = () => {
                 <div className="cart-items-title  cart-items-item">
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>৳{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
-                  <p>x</p>
+                  <p>৳{item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
                 </div>
 
                 <hr />
@@ -34,6 +36,37 @@ export const Cart = () => {
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>৳{0}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivary Fee</p>
+            <p>৳{2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Total</p>
+            <p>৳{0}</p>
+          </div>
+          <button className="checkout-btn">Proceed to Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>
+              If you have a promocode, please enter it below to receive your discount.
+            </p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="Enter promocode" />
+              <button>Apply</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
