2d0b1646449f4daf7247c4007ece70b709443a8a
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-27 12:33:24 +0600

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,4 +1,4 @@
-import foodModel from "../models/foodModel";
+import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,24 +1,21 @@
-import express from 'express';
-import { addFood } from '../controllers/foodController.js';
+import express from "express";
+import { addFood } from "../controllers/foodController.js";
 
-import multer from 'multer';
+import multer from "multer";
 
 const foodRouter = express.Router();
 
 //Image Storage engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}-${file.originalname}`)
+  },
+});
+
+const upload = multer({ storage: storage });
 
 
+foodRouter.post("/add", upload.single("image"), addFood);
 
-foodRouter.post("/add", addFood);   
-
-
-
-
-
-
-
-
-
-
-
-export default foodRouter;  
+export default foodRouter;
