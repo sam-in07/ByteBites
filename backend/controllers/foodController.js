27952af73205d6290f81e837447c8616beaf908c
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-02-27 15:14:24 +0600

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,11 +1,30 @@
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
-
 //add food itesm
 
-const addFood = async (req,res) => {
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
+  const food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+  try {
+    await food.save();
+    res.json({
+      success: true,
+      message: "Food item added successfully",
+    });
+  } catch (error) {
+    console.error("Error adding food item:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to add food item",
+    });
+  }
+};
 
-}
-
-export {addFood}    
+export { addFood };
