805cfc23cb8ceea316b5280615b7690e83dcf4b5
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-01 16:01:08 +0600

    commit

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -3,47 +3,49 @@ import "./Add.css";
 import { assets } from "../../assets/assets";
 const Add = () => {
   return (
-    <div>
-      <div className="add">
-        <form action="" className="flex-col">
-          <div className="add-img-upload flex col">
-            <p>Upload Image</p>
-            <label htmlFor="image">
-              <img src={assets.upload_area} alt="" />
-            </label>
-            <input type="file" id="image" hidden required />
-          </div>
-          <div className="add-product-name flex col">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name" required />
-          </div>
-          <div className="add-product-description flex col">
-            <p>Product Description</p>
-            <textarea
-              name="Description"
-              rows="6"
-              placeholder="Write content here"
-              required
-            />
-          </div>
-          <div className="add-catagory-price">
-            <div className="add-product-catagory flex col">
-              <p>Product Catagory</p>
-              <select name="catagory">
-                <option value="Salad">Salad</option>
-                <option value="Rolls">Rolls</option>
-                <option value="Deserts">Deserts</option>
-                <option value="Sandwich">Sandwich</option>
-                <option value="Cake">Cake</option>
-                <option value="Pure Veg">Pure Veg</option>
-                <option value="Pasta">Pasta</option>
-                <option value="Noodels">Noodels</option>
-              </select>
+   
+    <div className='add'>
+        <form className="flex-col" >
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor="image">
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input  type="file" id='image' hidden required />
             </div>
-          </div>
+            <div className="add-product-name flex-col">
+                <p>Product Name</p>
+                <input  type="text" name='name' placeholder='Type here' />
+                </div>
+                <div className='add-product-description flex-col'>
+                <p>Product description</p>
+                <textarea  name="description" rows="6" placeholder="Write content here" required></textarea>
+            </div>
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product category</p>
+                    <select  name="category" >
+                        <option value="Salad">Salad</option>
+                        <option value="Rolls">Rolls</option>
+                        <option value="Deserts">Deserts</option>
+                        <option value="Sandwich">Sandwich</option>
+                        <option value="Cake">Cake</option>
+                        <option value="Pure Veg">Pure Veg</option>
+                        <option value="Pasta">Pasta</option>
+                        <option value="Noodels">Noodels</option>
+                    </select>
+                </div>
+                <div className="add-price flex-col">
+                    <p>Product Price</p>
+                    <input  type="Number" name='price' placeholder='$20'/>
+                </div>
+            </div>
+                <button type='submit' className='add-btn'>Add</button>
         </form>
-      </div>
+
     </div>
+       
+    
   );
 };
 
