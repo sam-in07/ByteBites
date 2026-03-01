2a63a6c28ba51ec659b5e6384cca90d83334658f
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-01 08:43:29 +0600

    commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/Sidebar/Sidebar'
-
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,9 @@
-import React from "react";
-import "./Navbar.css";
-import { assets } from '../../assets/assets'
-
+import React from 'react'
+import './Navbar.css'   
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <img className="logo" src={assets.logo} alt="" />
-      <img className="profile" src={assets.profile_image} alt="" />
-    </div>
-  );
-};
+    <div>Navbar</div>
+  )
+}
 
-export default Navbar;
+export default Navbar
