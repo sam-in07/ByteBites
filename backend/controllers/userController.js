03cd592efd99f342a66e226085919a3947cccd99
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-02 10:41:22 +0600

    userrouter

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+import userModel from "../models/userModel";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+//login user 
+
+const loginUser = async (req, res) => {
+
+}
+
+//register
+
+const registerUser = async (req, res) => {
+
+
+}
+
+export { loginUser, registerUser };
