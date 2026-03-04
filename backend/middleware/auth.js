329c5d7b51fae5593903edfc816277b37098f4fe
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-04 08:13:05 +0600

    commit functions

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,7 +1,27 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-
-}
+  const token = req.headers;
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "No token provided",
+    });
+  }
+  try {
+    const token_decode = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+    );
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({
+        success: false,
+        message: "Invalid token",
+    })
+  }
+};
 
 export default authMiddleware;
