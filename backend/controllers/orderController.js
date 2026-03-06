7883552be4ce152eb72652341737ea232508e257
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-03-06 11:48:04 +0600

    sessions

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,6 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing order for
 
 const placeOrder = async (req, res) => {
+  const frontend_url = "http://localhost:5173";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -35,6 +36,12 @@ const placeOrder = async (req, res) => {
         unit_amount: 2 * 100,
       },
       quantity: 1,
+    });
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
     
   } catch (error) {}
